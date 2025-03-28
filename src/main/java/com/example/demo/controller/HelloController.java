cad4f8621985e5a0469c5cba39e400de5b05e0e7
sasireddy00 <sasid7984@gmail.com>
2025-03-28 17:45:18 +0530

    Version changed

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello, CI/CD with Jenkins!";
+        return "Hello, CI/CD with Jenkins from SASIDHAR REDDY!";
     }
 }
